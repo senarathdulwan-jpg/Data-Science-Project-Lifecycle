5eb98502556233697831aae517d94af2dc54beac
senarathdulwan-jpg <senarathdulwan@gmail.com>
2026-04-25 15:46:33 +0530

    Update Data Science Project Lifecycle Individual.py

--- Data Science Project Lifecycle Individual.py
+++ Data Science Project Lifecycle Individual.py
@@ -172,7 +172,7 @@ fig3 = px.bar(
     y='Country',
     orientation='h',
     color='Percentage',
-    color_continuous_scale='Blues_r'
+    color_continuous_scale='Viridis'
 )
 fig3.update_layout(xaxis_title="Exposure (%)")
 
